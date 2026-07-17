b4b975b55c8151f8670fe1aeef5a5cea624f3064
Karsing <charlie2561818@gmail.com>
2026-07-17 14:30:20 +0800

    Ignore TS and ESLint build errors

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Allow mobile testing via local IP
   allowedDevOrigins: ['192.168.30.37', 'localhost'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
